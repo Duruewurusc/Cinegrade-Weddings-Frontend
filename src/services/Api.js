8ead6603512ebc385b7d7e71df3085c3c34dee44
Duruewuru Chukwuemeka <duruewuruchukwuemeka@gmail.com>
2025-08-20 11:35:26 +0100

    live backend update

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const api = axios.create({
-  baseURL: 'cinegradeapi.onrender.com/'
+  baseURL: ''
   //'http://127.0.0.1:8000/',
   // baseURL: 'http://192.168.116.59:8000',
  
