d36c75d85293a43890fdcbc0b60a1066da0f00b8
Duruewuru Chukwuemeka <duruewuruchukwuemeka@gmail.com>
2025-11-20 20:50:53 +0100

    change to https

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 export const api = axios.create({
-  baseURL: 'http://api.cinegradevisuals.com'
+  baseURL: 'https://api.cinegradevisuals.com'
   // baseURL: 'https://cgapi-8nul.onrender.com'
   //'http://127.0.0.1:8000/',
   // baseURL: 'http://127.0.0.1:8000/',
